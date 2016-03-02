341710f59dd95a208dda0cad97da8b95bbb1d82c
Mark Fruman <majorgowan@yahoo.com>
2016-03-02 18:15:38 -0500

    forgot to update urls

--- imageReveal/urls.py
+++ imageReveal/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
         url(r'^$', views.index, name='index'),
         url(r'^clue$', views.serveClue, name='clue'),
         url(r'^hbte$', views.hbte, name='hbte'),
+        url(r'^highscores$', views.highscores, name='highscores'),
         ]
 
